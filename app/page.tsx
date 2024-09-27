885ee535a863128384c34df69531b64f8b31ada5
Pho Thin <phothinmg@disroot.org>
2024-09-28 01:18:35 +0630

    modified:   app/globals.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./home.module.css";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
-      <Link href={"/julian_date_converter"}>Converter</Link>
-    </div>
+    <section className={styles.hmdev}>
+      <div className={styles.home}>
+        <h1>MyCal</h1>
+        <hr />
+        <ol>
+          <li>
+            {" "}
+            <Link
+              href={"/julian_date_converter"}
+              target="_blank"
+              className={styles.lnk}
+            >
+              Julian Date Converter
+            </Link>
+          </li>
+        </ol>
+      </div>
+    </section>
   );
 }
