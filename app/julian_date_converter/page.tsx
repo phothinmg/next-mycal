baaf3872bf836d1e94c18e65ceb46cbe4ec05d03
Pho Thin <phothinmg@disroot.org>
2024-09-28 01:30:37 +0630

    modified:   app/julian_date_converter/page.tsx

--- app/julian_date_converter/page.tsx
+++ app/julian_date_converter/page.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import type { Metadata } from "next";
-import DateTimeToJulian from "./datetime_to_julian";
 import styles from "./converter.module.css";
-import JulianToDateTime from "./julian_to_datetime";
 import JulianConverter from "./converter";
-import ReactSocialIcon from "jsx-social-icons/react";
 export const metadata: Metadata = {
   title: "Julian Date Converter",
 };
